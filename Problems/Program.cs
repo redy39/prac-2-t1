83c5ef0926728bdcbb0c58053fdd7ace2dd5b7b5
redy <redy39@gmail.com>
2019-02-21 15:38:58 +0200

    Task1Q12

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,8 +2,18 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        string Prefix(string input)
+        {
+            int len = input.Length;
+            string[] arr = input.Split();
+            int numWords = arr.Length;
+            string output = Convert.ToString(len) + "," + Convert.ToString(numWords) + ":" + input;
+
+            return output;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
